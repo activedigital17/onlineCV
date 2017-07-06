d0802b8358f53bd860f545d31754c91f9ec06e29
Antonio <antonio.plantas@gmail.com>
2017-07-06 10:48:18 +0200

    make it bettrfdsfds

--- index.php
+++ index.php
@@ -275,7 +275,7 @@ padding-top: 20px;
                     <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
                     <li data-target="#myCarousel" data-slide-to="1"></li>
                     <li data-target="#myCarousel" data-slide-to="2"></li>
-                    <li data-target="#myCarousel" data-slide-to="3"></li>
+                    
                    
                   </ol>
 
